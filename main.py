29ed9ee98e4b842c29201294aabc9c3b7cf1f4c2
LucasLLopes02 <116230364+LucasLLopes02@users.noreply.github.com>
2022-11-23 23:56:02 +0000

    ok

---
+++ main.py
@@ -0,0 +1,9 @@
+#importarnnosso arquivo Pessoa.py no diretório model
+from model.Pessoa import Pessoa
+
+#Exemplo de uso
+poyatos = Pessoa (1, "Henrique Poyatos")
+print(poyatos)
+
+#Quero mostrar só o nome
+print(poyatos.nome)
